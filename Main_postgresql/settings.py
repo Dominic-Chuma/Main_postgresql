73ece13e175bc1ea92332e522eb3be1badf80034
Dominic Chuma <chumadominic@gmail.com>
2022-11-12 22:03:56 +0100

    Postgresql connection succeful, .gitignore & .env files created

--- Main_postgresql/settings.py
+++ Main_postgresql/settings.py
@@ -74,22 +74,23 @@ WSGI_APPLICATION = 'Main_postgresql.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'OPTIONS': {
-#             'service': 'my_service',
-#             'passfile': '.my_pgpass',
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':"mine",
+        'USER':"postgres",
+        'PASSWORD':"deduceme123#",
+        'HOST':"127.0.0.1",
+        'PORT':"5432",
+    }
+}
 
 
 # Password validation
