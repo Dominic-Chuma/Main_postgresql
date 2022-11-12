4267a23bc78c818208c21919285d6692bd29832c
Dominic Chuma <chumadominic@gmail.com>
2022-11-12 23:06:16 +0100

    dotenv file configured with environmet variables

--- Main_postgresql/settings.py
+++ Main_postgresql/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
+# Import the Environ package for the dotenv file...
+import environ
+
+# Initialize some env variables....
+env = environ.Env()
+environ.Env().read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sgu^!#e7*%a2x%^c!o#fi-(qws^-is!9v5$l9dx@0zuaeen^e='
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,11 +92,11 @@ WSGI_APPLICATION = 'Main_postgresql.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':"mine",
-        'USER':"postgres",
-        'PASSWORD':"deduceme123#",
-        'HOST':"127.0.0.1",
-        'PORT':"5432",
+        'NAME':env("DB_NAME"),
+        'USER':env("DB_USER"),
+        'PASSWORD':env("DB_PASSWORD"),
+        'HOST':env("DB_HOST"),
+        'PORT':env("DB_PORT"),
     }
 }
 
