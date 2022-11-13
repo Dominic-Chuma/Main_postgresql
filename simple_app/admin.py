fba8733b327ae6bb9ffeb9791a05057659d079bd
Dominic Chuma <chumadominic@gmail.com>
2022-11-13 07:02:51 +0100

    Models have been registered in the admin.py

--- simple_app/admin.py
+++ simple_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from simple_app.models import ActionList, ActionRecord, User
 
 # Register your models here.
+admin.site.register(ActionList)
+admin.site.register(ActionRecord)
+admin.site.register(User)
